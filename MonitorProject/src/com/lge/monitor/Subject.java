9139f0fec60f3097f051f28fe3b91e9b8979981f
Byungseon Shin <sun.shin@lge.com>
2014-05-27 21:18:20 -0400

    Remove testing files

--- MonitorProject/src/com/lge/monitor/Subject.java
+++
@@ -1,9 +0,0 @@
-package com.lge.monitor;
-
-import java.util.Observable;
-
-public abstract class Subject extends Observable{
-	String status; 
-	abstract public String getStatus();
-	abstract public void setStatus(String status);
-}
