5775d670208e36b9de9972cc5ab789232a0c3c6e
tommy.park <tommy.park@tommypark.LGE.NET>
2014-05-28 10:53:17 +0900

    CustomerApp: CustomerMain
    - Create empty projects.
    - Please setup your own classpath

---
+++ CustomerApplicationProject/src/com/lge/spartan/customer/CustomerApplicationMain.java
@@ -0,0 +1,9 @@
+package com.lge.spartan.customer;
+
+public class CustomerApplicationMain {
+
+	public static void main(String[] args) {
+		System.out.println("CustomerApplication Start....");
+	}
+
+}
---
+++ WarehouseProject/src/com/lge/spartan/warehouse/WarehouseMain.java
@@ -0,0 +1,9 @@
+package com.lge.spartan.warehouse;
+
+public class WarehouseMain {
+
+	public static void main(String[] args) {
+		System.out.println("Warehouse System Start.... ");
+	}
+
+}
