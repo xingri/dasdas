cb2f5196633584cb027e9ec3e37ab12601c5e853
Byungseon Shin <sun.shin@lge.com>
2014-06-09 18:54:01 -0400

    [Warehouse Server] Initial implementation of Warehouse Server
    Warehouse Server implementation uploaded.
    It is based on Static Factory Method and there are 4 different Warehouse Workers.
    1. Inventory Switch Worker
    2. Inventory Sensor Worker
    3. Shipping Switch Worker
    4. Shipping Sensor Worker

--- WarehouseHW/StationManager/run.sh
+++ WarehouseHW/StationManager/run.sh
@@ -1,3 +1,3 @@
-javac -classpath .:./lib/RXTXcomm.jar SerialInput.java  RobotControl.java StationManager.java
+javac -classpath .:./lib/RXTXcomm.jar SerialInput.java  RobotControl.java StationManager.java HWStationManagerImpl.java
 #java -Djava.library.path=./lib -cp .:./lib/RXTXcomm.jar StationManager $1
-java -Djava.library.path=./lib -cp .:./lib/RXTXcomm.jar StationManager 128.237.124.48
+java -Djava.library.path=./lib -cp .:./lib/RXTXcomm.jar HWStationManagerImpl 128.237.124.48
--- WarehouseServer/src/com/lge/spartan/warehouse/WarehouseMain.java
+++ WarehouseServer/src/com/lge/spartan/warehouse/WarehouseMain.java
@@ -1,9 +1,49 @@
-package com.lge.spartan.warehouse;
+//package com.lge.spartan.warehouse;
 
-public class WarehouseMain {
+public class WarehouseMain implements StationManager {
 
-	public static void main(String[] args) {
-		System.out.println("Warehouse System Start.... ");
-	}
+    public void eventHandler(String inputLine) {
+        if(inputLine != null) {
+            WHWorker whWorker = WHWorkerFactory.create(inputLine);
+            if(whWorker != null) {
+                whWorker.procRequest();
+            } else {
+                System.out.println("Cannot create Warehouse Worker, check input [" + inputLine + "]!!!");
+            }
+        } else {
+            System.out.println("invalid data comming from warehouse hardware [" + inputLine + "]!!!");
+        }
+    }
 
+    public static void main(String[] argv) {
+
+        System.out.println("Warehouse System Start.... ");
+
+        if (argv.length == 0) {
+            System.out.println ( "\nPlease specify an IP address on the command line.\n" );
+            System.exit(1);
+        } else {
+            System.out.println ( "\n\nUsing server " + argv[0] + ".\n" );
+        }
+
+        WarehouseMain whMain = new WarehouseMain();
+
+        SerialInput serialInput = new SerialInput();
+        serialInput.setStationManager(whMain);
+        serialInput.initialize();
+
+        WHStartWorker startWorker = new WHStartWorker();
+        startWorker.procRequest();
+
+        Thread t=new Thread() {
+            public void run() {
+                //the following line will keep this app alive for 10000 seconds,
+                //waiting for events to occur and responding to them (printing incoming messages to console).
+                try {Thread.sleep(10000000);} catch (InterruptedException ie) {}
+            }
+        };
+
+        t.start();
+        System.out.println("Started");
+    }
 }
