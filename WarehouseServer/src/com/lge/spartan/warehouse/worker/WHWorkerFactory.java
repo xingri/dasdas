373f274f3b6c133a3a738b185951262322c3259a
Byungseon Shin <sun.shin@lge.com>
2014-06-10 12:07:46 -0400

    [Warehouse Server] Re-order Switch / Sensor detction sequence
    Switch should have more priority

--- WarehouseServer/src/com/lge/spartan/warehouse/worker/WHWorkerFactory.java
+++ WarehouseServer/src/com/lge/spartan/warehouse/worker/WHWorkerFactory.java
@@ -5,14 +5,14 @@ public class WHWorkerFactory {
         }
 
         WHEvent whEvt = new WHEvent(eventInput);
-        if ( whEvt.isSSSensor() ) {
-            return new SSSensorWorker();
-        } else if ( whEvt.isSSSwitch() ) {
+        if ( whEvt.isSSSwitch() ) {
             return new SSSwitchWorker();
-        } else if ( whEvt.isISSensor() ) {
-            return new ISSensorWorker();
         } else if ( whEvt.isISSwitch() ) {
             return new ISSwitchWorker();
+        } else if ( whEvt.isSSSensor() ) {
+            return new SSSensorWorker();
+        } else if ( whEvt.isISSensor() ) {
+            return new ISSensorWorker();
         } else {
             System.out.println("unknown Event Type");
             return null;
