0f5ca1762bddc18f6e24a2f0a6e89da00d407419
Byungseon Shin <sun.shin@lge.com>
2014-06-10 23:27:24 -0400

    [Warehouse Server] Sync with Station Manager Rev 0.9

--- WarehouseServer/src/com/lge/spartan/warehouse/run.sh
+++ WarehouseServer/src/com/lge/spartan/warehouse/run.sh
@@ -1,4 +1,4 @@
 cd worker;./run.sh;cd -
 javac -classpath .:./worker:../../../../../../WarehouseHW/StationManager/lib/RXTXcomm.jar:../../../../../../WarehouseHW/StationManager  WarehouseMain.java
 
-#java -Djava.library.path=../../../../../../WarehouseHW/StationManager/lib -cp .:./worker:../../../../../../WarehouseHW/StationManager/lib/RXTXcomm.jar:../../../../../../WarehouseHW/StationManager/ WarehouseMain 128.237.124.48
+java -Djava.library.path=../../../../../../WarehouseHW/StationManager/lib -cp .:./worker:../../../../../../DAL/dist/DAL.jar:../../../../../../DAL/jdbc/mysql/mysql-connector-java-5.1.30-bin.jar:../../../../../../WarehouseHW/StationManager/lib/RXTXcomm.jar:../../../../../../WarehouseHW/StationManager/ WarehouseMain 128.237.124.48
--- WarehouseServer/src/com/lge/spartan/warehouse/worker/run.sh
+++ WarehouseServer/src/com/lge/spartan/warehouse/worker/run.sh
@@ -1,1 +1,1 @@
-javac ISSensorWorker.java   SSSensorWorker.java WHEvent.java        WHWorker.java ISSwitchWorker.java SSSwitchWorker.java    WHStartWorker.java  WHWorkerFactory.java
+javac ISSensorWorker.java SSSensorWorker.java WHWorker.java ISSwitchWorker.java SSSwitchWorker.java WHStartWorker.java WHWorkerFactory.java
