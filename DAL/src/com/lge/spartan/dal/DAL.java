98eb8925162e520ae7a2f438341c0dadc3cce92e
vijayra <vijay.rachabattuni@lge.com>
2014-06-19 01:24:21 +0530

    [DAL] Empty spaces removed in DAL.java

--- DAL/src/com/lge/spartan/dal/DAL.java
+++ DAL/src/com/lge/spartan/dal/DAL.java
@@ -23,41 +23,27 @@ import java.util.List;
  * @author vijay.rachabattuni
  */
 public interface DAL {
-
     //Init and Uninit DB
     public boolean Initialize(String serverIPAddress, String userName, String pwd);
-
     public boolean Uninitialize();
 
     //Orders - Start
     public int AddOrder(List<OrderDetails> orderList, Customer cust);
-
     public ArrayList<OrderInfo> GetOrders(OrderStatus orderStatus);
-
     public OrderStatus GetOrderStatus(int orderNo);
-
     public ArrayList<OrderInfo> GetOrdersByPhone(String phone/* Enum orderStatus*/);
-
     public boolean UpdateOrderStatus(int orderNo, Enum orderStatus);
-
     public OrderInfo PickFirstOrder();
     //Orders - End
 
     //Widgets - Start
     public int AddWidget(String widgetName, String widgetDesc, int quant, int stationId);
-
     public int IncWidgets(String widgetName, int increment);
-
     public int DecWidgets(String widgetName, int decrement);
-
     public int GetWidgetQuantity(String widgetName);
-
     public ArrayList<Widget> GetWidgets();
-
     public int GetWidgetStation(int widgetId);
-
     public int DecWidgets(int widgetId, int decrement);
-
     public int GetWidgetQuantity(int widgetId);
     //Widgets - End
     
@@ -67,24 +53,16 @@ public interface DAL {
     
     //Robot - Start
     public int AddRobotStatus(RobotStatus robotStatus);
-
+    //Obsolete API. Will be removed later. Use GetRobotMoves instead
   //  public RobotStatus GetRobotStatus(int orderNo);
-
     public int UpdateRobotStatus(RobotStatus robotStatus);
-    
     public ArrayList<Robot> GetRobots();
-    
     public RobotState GetRobotState(int robotId);
-    
     public boolean SetRobotState(int robotId, RobotState rs);
-    
     public RobotStatus GetRobotMoves(int robotId, int orderId);
-    
     //Robot - End
     
     public ArrayList<Station> GetStations();
-    
     public ArrayList<Warehouse> GetWarehouses();
-    
     public boolean IsDBAvailable();
 }
