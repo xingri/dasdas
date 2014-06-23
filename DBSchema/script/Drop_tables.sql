aee4bd84dc98a5075ed4388c6475a7ca25e02533
Byungseon Shin <sun.shin@lge.com>
2014-06-22 22:59:05 -0400

    [DB Schema] Update Drop Table script according to v.11

--- DBSchema/script/Drop_tables.sql
+++ DBSchema/script/Drop_tables.sql
@@ -2,7 +2,10 @@ USE `spartan`;
 
 drop table orderdetails;
 drop table widget;
-drop table warehouse;
 drop table robotstatus;
+drop table robotmoves;
+drop table robot;
 drop table orderinfo;
 drop table customer;
+drop table station;
+drop table warehouse;
